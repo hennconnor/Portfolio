e2767b1dbc5e1b12bab33a1431a61ac0dcbc9190
Connor Henn <connor.henn@yahoo.com>
2023-04-29 17:46:08 -0700

    mobile nav and cropped picture

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,16 @@ import { BsFillPersonLinesFill } from 'react-icons/bs';
 import Logo from '../assets/Logo.png';
 import Resume from '../assets/Resume.pdf';
 import { Link } from 'react-scroll';
+import React, { useState } from 'react';
 
 
 const Navbar = () => {
+
+    const [nav, setNav] = useState(false);
+
+    const handleClick = () => {
+        setNav(!nav);
+    }
     return (
         <div className='w-full h-[80px] fixed flex justify-between items-center px-4 bg-[#0a192f] text-white'>
             <img src={Logo} alt='Logo' width='80' height='80' />
@@ -27,9 +34,29 @@ const Navbar = () => {
                 </li>
             </ul>
 
-            <div className='md:hidden'>
-                <FaBars />
+            <div className='md:hidden cursor-pointer z-10'>
+                {nav ? <FaTimes onClick={handleClick} /> : <FaBars onClick={handleClick} />}
             </div>
+
+            {/* Mobile Menu */}
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
+                <li className='py-6 text-4xl cursor-pointer hover:underline'>
+                    <Link to='home' spy={true} smooth={true} offset={10} duration={500} onClick={handleClick}>Home</Link>
+                </li>
+                <li className='py-6 text-4xl cursor-pointer hover:underline'>
+                    <Link to='about' spy={true} smooth={true} offset={-150} duration={500} onClick={handleClick}>About</Link>
+                </li>
+                <li className='py-6 text-4xl cursor-pointer hover:underline'>
+                    <Link to='skills' spy={true} smooth={true} offset={-150} duration={500} onClick={handleClick}>Skills</Link>
+                </li>
+                <li className='py-6 text-4xl cursor-pointer hover:underline'>
+                    <Link to='projects' spy={true} smooth={true} offset={-150} duration={500} onClick={handleClick}>Projects</Link>
+                </li>
+                <li className='py-6 text-4xl cursor-pointer hover:underline'>
+                    <Link to='contact' spy={true} smooth={true} offset={100} duration={500} onClick={handleClick}>Contact</Link>
+                </li>
+            </ul>
+
             <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
                 <ul>
                     <li className="bg-blue-400 w-[160px] h-[60px] flex justify-between cursor-pointer items-center ml-[-100px] px-4 hover:ml-[-10px] duration-300">
