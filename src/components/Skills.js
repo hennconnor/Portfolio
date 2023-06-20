f508b78e81fbc037c53191138b2e61e398a62636
Connor Henn <connor.henn@yahoo.com>
2023-06-20 14:08:20 -0700

    updated skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,13 +4,16 @@ import HTMLLogo from '../assets/Icons/html.png';
 import ReactLogo from '../assets/Icons/React.png';
 import GitLogo from '../assets/Icons/git.png';
 import RailsLogo from '../assets/Icons/Rails.png';
+import TypescriptLogo from '../assets/Icons/Typescript.png';
+import TailwindLogo from '../assets/Icons/Tailwind.png';
+
 
 const Skills = () => {
     return (
         <div name='skills'>
             <div className='flex flex-col justify-center items-center w-max-[1000px] mb-64'>
                 <p className='text-5xl mb-5'>Skills</p>
-                <ul className='flex flex-row flex-wrap justify-center items-center font-mono'>
+                <ul className='flex flex-row flex-wrap justify-center items-center font-mono max-w-[680px]'>
                     <li className='bg-[#0a192f] w-36 h-36 shadow-lg shadow-black flex flex-col justify-center items-center mx-2 my-2'>
                         <p>JavaScript</p>
                         <img src={JSLogo} alt='JavaScript Logo' height='50px' width='60px' />
@@ -26,6 +29,14 @@ const Skills = () => {
                     <li className='bg-[#0a192f] shadow-lg shadow-black w-36 h-36 flex flex-col justify-center items-center mx-2 my-2'>
                         <p>React</p>
                         <img src={ReactLogo} alt='React Logo' height='75px' width='75px' />
+                    </li>
+                    <li className='bg-[#0a192f] shadow-lg shadow-black w-36 h-36 flex flex-col justify-center items-center mx-2 my-2'>
+                        <p>Tailwind</p>
+                        <img src={TailwindLogo} alt='Tailwind Logo' height='75px' width='75px' />
+                    </li>
+                    <li className='bg-[#0a192f] shadow-lg shadow-black w-36 h-36 flex flex-col justify-center items-center mx-2 my-2'>
+                        <p>Typescript</p>
+                        <img src={TypescriptLogo} alt='Typescript Logo' height='75px' width='75px' />
                     </li>
                     <li className='bg-[#0a192f] shadow-lg shadow-black w-36 h-36 flex flex-col justify-center items-center mx-2 my-2'>
                         <p>Ruby on Rails</p>
