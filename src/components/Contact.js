370728521e39e7203f012179d65bda75836cecdf
Connor Henn <connor.henn@yahoo.com>
2023-12-15 18:26:10 -0800

    improved project and contact page for smaller screen

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,9 +12,9 @@ const Contact = () => {
     return (
         <div name='contact' className="flex flex-col justify-center items-center mb-20 font-poppins">
 
-            <div className='flex flex-col justify-center items-center w-[85%] max-h-[600px] max-w-[800px] bg-white rounded-md border-2 border-gray-500 shadow-black shadow-lg  px-5 py-4'>
-                <h1 className='text-3xl text-center mb-5 underline underline-offset-8'>Send me a Message</h1>
-                <div className='flex justify-center items-center bg-gray-200 rounded-full w-[100px] h-[100px] text-black'><FaEnvelope size={40} /></div>
+            <div className='flex flex-col justify-center items-center max-h-[600px] min-w-[370px] mx-1 max-w-[800px] bg-white rounded-md border-2 border-gray-500 shadow-black shadow-lg  px-5 py-4'>
+                <h1 className='text-3xl text-center mb-5'>Let's Connect!</h1>
+                <div className='flex justify-center items-center bg-gray-200 rounded-full w-[100px] h-[100px] text-black mb-2'><FaEnvelope size={40} /></div>
                 <form ref={myRef} className='flex flex-col p-2 w-full text-black' action='https://getform.io/f/cb6288aa-aa41-4da0-a612-e3f677d74712' method="POST">
                     <input className='p-2 border-gray-300 border-solid border-2' type='text' placeholder='Name' name='name' required></input>
                     <input className='my-2 p-2 border-gray-300 border-solid border-2' type='email' placeholder='Email' name='email' required></input>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,10 +7,10 @@ const Projects = () => {
                 <p className=' text-white text-5xl mb-5 underline underline-offset-2'>Projects</p>
                 <div className='flex flex-wrap justify-evenly'>
 
-                    <div className='bg-white rounded-md border-2 border-gray-500 flex flex-col max-w-[400px] max-h-[750px] shadow-lg shadow-black p-3'>
+                    <div className='bg-white rounded-md border-2 border-gray-500 flex flex-col max-w-[450px] min-w-[370px] mx-1 max-h-[600px] shadow-lg shadow-black p-3'>
                         <p className='text-lg'>Plus Exp</p>
-                        <img className='border-2 border-black m-2' src={PlusExpPic} alt='Plus Exp Project' />
-                        <p className='justify-center p-5'>Created a gamified daily task planner where users gain experience points for completing tasks. Experience points are used to increase user's level with the goal to increase incentives and make the process of completing daily tasks more enjoyable.</p>
+                        <img className='border-2 border-black mt-2 mx-2' src={PlusExpPic} alt='Plus Exp Project' />
+                        <p className='justify-center p-5 text-center'>Created a gamified daily task planner where users gain experience points for completing tasks. Experience points are used to increase user's level with the goal to increase incentives and make the process of completing daily tasks more enjoyable.</p>
                         <a className='min-w-[122px] self-center justify-self-center border-2 p-3 hover:bg-gray-100 hover:rounded-md duration-50' href='https://github.com/hennconnor/PlusExp' target='_blank' rel='noopener noreferrer'>Source Code</a>
                         <a className='my-2 min-w-[122px] self-center justify-self-center border-2 p-3 hover:bg-gray-100 hover:rounded-md duration-50' href=' https://www.loom.com/share/da5e1f9b808c48148f4f182f2965b9e4?sid=d57d85a0-c89a-4cc7-a31f-b2a45eb2f552' target='_blank' rel='noopener noreferrer'>Video Demo</a>
                     </div>
