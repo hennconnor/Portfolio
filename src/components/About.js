962fe1fd610c22dd0c1274e947bfb6589fa9e4ed
Connor Henn <connor.henn@yahoo.com>
2023-12-15 18:41:38 -0800

    updated about

--- src/components/About.js
+++ src/components/About.js
@@ -4,9 +4,8 @@ const About = () => {
             <div className='flex flex-col justify-center items-center px-8 font-poppins'>
                 <h1 className='text-5xl mb-10 text-white underline underline-offset-2'>About</h1>
                 <div className='border-gray-500 text-2xl min-w-[300px] max-w-[680px] text-center text-white'>
-                    Hi, my name is Connor. I am a full stack software engineer looking to get into the professional world of coding.
-                    I am well-versed in JavaScript, TypeScript, React, Next, Ruby, Rails, HTML and CSS.
-                    Currently, I am a contract engineer and working on personal projects to further develop my skills.
+                    I am a full stack software engineer well-versed in JavaScript, TypeScript, React, Next, Ruby, Rails, HTML and CSS.
+                    Currently, I am working as contract engineer, while coding on the side to further develop my skills.
                     In my free time, I enjoy making music, socializing and going to the gym.
                     Follow me on my coding journey.
                 </div>
